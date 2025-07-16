3158da9dc23b90950fdf68fbf3002aeffc8a5d42
Ignacio Rivas Sosa <irivas@devsys.com.uy>
2025-07-16 15:47:17 -0300

    authenticacion de usuario para request v2.6

--- src/app/api/api-keys/[id]/route.ts
+++ src/app/api/api-keys/[id]/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server';
 import { getServerSession } from 'next-auth/next';
 import { supabase } from '../../../../lib/supabaseClient';
-import { authOptions } from '../../auth/[...nextauth]/route';
+//import { authOptions } from '../../auth/[...nextauth]/route';
 
 // Helper to get user_id from session
 async function getUserIdFromSession() {
